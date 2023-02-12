a0a3eaf9e36c913908c46f9153c4e262407d4bd2
olpol <omwfoster@gmail.com>
2023-02-12 19:23:01 +0000

    usb connection back
    not sure what happened there. revert files  worked. usb recovered.
    
    Thought problem might be removal of msp (weak linkage to stub). But no.

--- src/main.cpp
+++ src/main.cpp
@@ -131,18 +131,8 @@ int main(void)
   /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
 
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
   SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
   MX_GPIO_Init();
   uint8_t wheel_pos = 0;
 
